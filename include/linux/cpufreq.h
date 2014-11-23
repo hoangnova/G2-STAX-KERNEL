9e119c6a5e105f89da33382400346ac954e72a97
danielstuart14 <daniel.stuart14@gmail.com>
2014-11-23 18:15:08 -0200

    Revert "Added Alucard Gov"
    This reverts commit 79068cc96be28f7123e05ef290d096dae71f0228.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -395,9 +395,6 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
 extern struct cpufreq_governor cpufreq_gov_optimax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_optimax)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
-extern struct cpufreq_governor cpufreq_gov_alucard;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #endif
 
 
