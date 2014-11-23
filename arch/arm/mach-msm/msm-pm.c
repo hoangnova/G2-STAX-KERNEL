0d94e04c6c451af02e1a231a61c92a0de62e15c3
danielstuart14 <daniel.stuart14@gmail.com>
2014-11-23 19:35:48 -0200

    Revert "msm: msm-pm: Remove timeout waiting for cpu to power collapse"
    This reverts commit 0d1f875ec4f91e2f82b6a0fbfac117152f038250.

--- arch/arm/mach-msm/msm-pm.c
+++ arch/arm/mach-msm/msm-pm.c
@@ -829,7 +829,7 @@ int msm_pm_wait_cpu_shutdown(unsigned int cpu)
 		return 0;
 	if (!msm_pm_slp_sts[cpu].base_addr)
 		return 0;
-	while (1) {
+	while (timeout--) {
 		/*
 		 * Check for the SPM of the core being hotplugged to set
 		 * its sleep state.The SPM sleep state indicates that the
@@ -840,9 +840,10 @@ int msm_pm_wait_cpu_shutdown(unsigned int cpu)
 		if (acc_sts & msm_pm_slp_sts[cpu].mask)
 			return 0;
 		udelay(100);
-		WARN(++timeout == 50, "CPU%u didn't collapse in 5 ms\n", cpu);
 	}
 
+	pr_info("%s(): Timed out waiting for CPU %u SPM to enter sleep state",
+		__func__, cpu);
 	return -EBUSY;
 }
 
